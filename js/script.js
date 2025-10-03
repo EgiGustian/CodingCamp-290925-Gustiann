2109aaa596f56a8ffb7a957bf21b2ee3dc2733bf
Egi Gustian Maulana <119639520+EgiGustian@users.noreply.github.com>
2025-10-04 06:09:12 +0700

    menambahkan desain dan fix codingan

--- js/script.js
+++ js/script.js
@@ -1,41 +1,33 @@
-console.log("Hello, World!");
-
 let todos = [];
 
 // Menambahkan todo baru
 function addTodo(event) {
   // Mencegah refresh halaman
-  event.preventDefault(); 
-  
+  event.preventDefault();
   const todoInput = document.getElementById("todo-input");
   const todoDate = document.getElementById("todo-date");
-
   // validasi input
   if (validateInput(todoInput.value, todoDate.value)) {
-    let todo = ({ task: todoInput.value, date: todoDate.value });
+    let todo = { task: todoInput.value, date: todoDate.value };
     todos.push(todo);
-    console.log(todos);
-
+    // console.log(todos);
     // merender ulang daftar todo
     renderTodos();
-    
   }
 }
 
 function renderTodos() {
   const todoList = document.getElementById("todo-list");
-  
   // Hapus isi todoList sebelum merender ulang
-  todoList.innerHTML = '';
-
+  todoList.innerHTML = "";
   // Merender setiap todo
   todos.forEach((todo, index) => {
-    todoList.innerHTML += `<li class="border p-2 mb-2 flex justify-between items-center">
+    todoList.innerHTML += `<li class="border-2 border-indigo-600 rounded-lg flex p-2 mb-2 justify-between items-center shadow-xl">
     <div>
       <p class="font-bold">${todo.task}</p>
-      <p class="text-sm text-gray-500">${todo.date}</p>
+      <p class="text-sm text-white">${todo.date}</p>
     </div>
-    <button class="bg-red-500 text-white p-1 rounded" onclick="deleteTodo(${index})">Delete</button>
+    <button class="bg-red-500 text-white hover:bg-red-700 p-1 rounded" onclick="deleteTodo(${index})">Delete</button>
     </li>`;
   });
 }
@@ -44,7 +36,6 @@ function renderTodos() {
 function deleteTodo(index) {
   // Menghapus todo dari array berdasarkan index
   todos.splice(index, 1);
-
   // Merender ulang daftar todo
   renderTodos();
 }
@@ -55,20 +46,36 @@ function deleteAllTodo() {
   renderTodos();
 }
 
+// Filter todos berdasarkan tanggal
 function filterTodos() {
-
-  // menampilkan todo dengan tanggal tertentu
-  const filteredTodos = todos.filter(todo => todo.date === '2025-08-10');
-  console.log(filteredTodos);
-  // Anda bisa menampilkan filteredTodos di UI sesuai kebutuhan
+  const calendarInput = document.getElementById("calendarInput");
+  const selectedDate = calendarInput.value;
+  // validasi input tanggal
+  if (!selectedDate) {
+    alert("Silakan pilih tanggal untuk memfilter.");
+    return;
+  }
+  // Filter todos berdasarkan tanggal yang dipilih
+  const filteredTodos = todos.filter((todo) => todo.date === selectedDate);
+  const todoList = document.getElementById("todo-list");
+  todoList.innerHTML = "";
+  filteredTodos.forEach((todo, index) => {
+    todoList.innerHTML += `<li class="border-2 border-indigo-600 rounded-lg flex p-2 mb-2 justify-between items-center shadow-xl">
+      <div>
+        <p class="font-bold">${todo.task}</p>
+        <p class="text-sm text-white">${todo.date}</p>
+      </div>
+      <button class="bg-red-500 text-white hover:bg-red-700 p-1 rounded" onclick="deleteTodo(${index})">Delete</button>
+    </li>`;
+  });
 }
 
 // validate input fields
-function validateInput(todo, date) {
+function validateInput(todo, date, dateFilter) {
   // Cek apakah input kosong
-  if (todo === '' || date === '') {
+  if (todo === "" || date === "" || dateFilter === "") {
     // Tampilkan pesan kesalahan
-    alert("Please fill in both fields.");
+    alert("Tidak boleh ada input yang kosong!");
     return false;
   }
   // Jika validasi berhasil
